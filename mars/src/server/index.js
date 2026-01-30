388822651c30a66d89bbb83524cd0411de1e9b94
zarifmahir <zarifmahir21@gmail.com>
2026-01-30 19:51:48 +0600

    Added db and index.js

--- mars/src/server/index.js
+++ mars/src/server/index.js
@@ -1,2 +1,20 @@
 // server backend
 
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const pool = require("./db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users");
+  res.json(result.rows);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port", process.env.PORT);
+});
