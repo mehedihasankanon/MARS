bdd09be610d6eca6d609ef6361a08e5becd3e222
Mehedi Hasan Kanon <mehedi.hkanon@gmail.com>
2026-01-30 20:18:52 +0600

    trying to solve porstgres connction issue

--- mars/src/database/db.js
+++ mars/src/database/db.js
@@ -1,5 +1,7 @@
 const { Pool } = require("pg");
 
+require("dotenv").config({ path: "../../.env.local" });
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
