34aa488c7b730e54d56d1ed111c0223d4b9cf22e
zarifmahir <zarifmahir21@gmail.com>
2026-01-30 20:45:11 +0600

    fixed port problem

--- mars/src/database/db.js
+++ mars/src/database/db.js
@@ -1,7 +1,5 @@
 const { Pool } = require("pg");
 
-require("dotenv").config({ path: "../../.env.local" });
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
